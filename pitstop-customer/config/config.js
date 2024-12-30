6d419336b075ddb2fc67bada116c2aa03e9aabe7
Hatim <hatim.slimani.19@gmail.com>
2024-12-30 21:56:13 +0100

    Fixing sum bugs 2

--- pitstop-customer/config/config.js
+++ pitstop-customer/config/config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = {
     development: {
-      username: process.env.DB_USER,
+      username: process.env.DB_USER ,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       host: process.env.DB_HOST,
